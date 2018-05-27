146e402d19a5361c093658ef69c7b75f34ce9072
Little-Flower-171 <36354459+Little-Flower-171@users.noreply.github.com>
2018-05-27 16:58:07 +0800

    Create reciprocal.cpp

---
+++ 2/reciprocal.cpp
@@ -0,0 +1,11 @@
+// reciprocal
+
+#include <iostream>
+int main()
+{
+    std::cout << "1/2 = 0.5\n"
+              << "1/1 = 1\n"
+              << "1/0 =\n";
+    std::cerr << "The denominator is 0!\n"
+              << "Is your brain impaired, Little Flower?\n";
+}
