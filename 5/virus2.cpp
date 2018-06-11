f20d7fc785f645e09accaf20326b4b31623f41d4
Little-Flower-171 <36354459+Little-Flower-171@users.noreply.github.com>
2018-06-11 18:48:29 +0800

    Create virus2.cpp

---
+++ 5/virus2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int count = 0;
+    for (char c; std::cin.get(c);) {
+        switch (c) {
+        default:
+            std::cout << c;
+            break;
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+            std::cout << 'x'; ++count;
+            break;
+        case 'A': case 'E': case 'I': case 'O': case 'U':
+            std::cout << 'X'; ++count;
+            break;
+    }
+    std::cout << "X rxplxcxd " << count << " vxwxl lxttxrs wxth x \"x\".\n"; //note the use of escape characters    
+}
